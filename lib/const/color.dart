b72ee3aac2018a8f6d7edccda7aa160bc124cc86
MuyoNz <pattaraponler@gmail.com>
2021-05-04 09:09:50 +0700

    const color

---
+++ lib/const/color.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFF3F9F1);
+  static const MaterialColor green = MaterialColor(0xFFE4F0E2, <int, Color>{
+    500: Color(0xFFE4F0E2),
+    600: Color(0xFF778B73),
+  });
+}
