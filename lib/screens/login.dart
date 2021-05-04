8afa11e568ca66101601607cba8b1201c1fc5d2f
MuyoNz <pattaraponler@gmail.com>
2021-05-04 09:13:17 +0700

    structure login screen

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_login/const/color.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -6,8 +7,27 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("LOGIN"),
+      backgroundColor: AppColors.background,
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.only(top: 16, bottom: 24),
+              child: Image.asset(
+                "assets/cover.png",
+                height: MediaQuery.of(context).size.height * 0.4,
+              ),
+            ),
+            Container(
+              height: MediaQuery.of(context).size.height * 0.4,
+              margin: EdgeInsets.symmetric(horizontal: 24),
+              decoration: BoxDecoration(
+                color: AppColors.green[500],
+                borderRadius: BorderRadius.circular(16),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
