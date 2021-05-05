c74729f8e283f11e73ba297fb654930092c895c7
Boonsong Srithong <song@odds.team>
2021-05-05 21:19:12 +0700

    add test username validator

---
+++ test/screens/login/login_controller_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:simple_login/screens/login/login_controller.dart';
+
+main() {
+  test("valid useranme", () {
+    var controller = LoginController();
+    final actual = controller.usernameValidator("");
+    expect(actual, "กรุณาระบุชื่อบัญชี");
+  });
+}
