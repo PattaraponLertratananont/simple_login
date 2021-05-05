578b7af8de7c1c2b2e9be3c028fd30cb91a5ef52
MuyoNz <pattaraponler@gmail.com>
2021-05-05 15:18:35 +0700

    login screen
    - validate

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -9,6 +9,10 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController usernameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,22 +23,122 @@ class _LoginScreenState extends State<LoginScreen> {
             Image.asset("assets/cover.png"),
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height * 0.4,
               margin: EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-              padding: EdgeInsets.all(16),
+              padding: EdgeInsets.all(24),
               decoration: BoxDecoration(
                 color: AppColors.green[500],
+                borderRadius: BorderRadius.circular(8),
               ),
-              child: Column(
-                children: [
-                  Text(
-                    "MyApp",
-                    style: TextStyle(
-                      fontSize: 22,
-                      fontWeight: FontWeight.bold,
+              child: Form(
+                key: formKey,
+                child: Column(
+                  children: [
+                    Text(
+                      "MyApp",
+                      style: TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: EdgeInsets.only(left: 24, right: 24),
+                      child: TextFormField(
+                        controller: usernameController,
+                        cursorColor: AppColors.green[600],
+                        decoration: InputDecoration(
+                          labelText: "ชื่อบัญชี",
+                          labelStyle: TextStyle(
+                            color: AppColors.green[600],
+                          ),
+                          hintText: "ใส่ชื่อนะ",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.green[600]!,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.green[600]!,
+                            ),
+                          ),
+                        ),
+                        validator: (username) {
+                          if (username!.isEmpty) {
+                            return "กรุณาระบุชื่อบัญชี";
+                          } else if (username.length <= 4) {
+                            return "ชื่อบัญชีสั้นเกินไป";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(left: 24, right: 24),
+                      child: TextFormField(
+                        obscureText: true,
+                        controller: passwordController,
+                        cursorColor: AppColors.green[600],
+                        decoration: InputDecoration(
+                          labelText: "รหัสผ่าน",
+                          labelStyle: TextStyle(
+                            color: AppColors.green[600],
+                          ),
+                          hintText: "ใส่รหัสนะ",
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.green[600]!,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: AppColors.green[600]!,
+                            ),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          formKey.currentState!.validate();
+                        },
+                        validator: (password) {
+                          RegExp regExp = RegExp("[0-9]");
+                          if (password!.isEmpty) {
+                            return "กรุณาระบุรหัสผ่าน";
+                          } else if (!regExp.hasMatch(password)) {
+                            return "รหัสผ่านไม่ถูกต้อง";
+                          } else if (password.length <= 6) {
+                            return "รหัสผ่านสั้นเกินไป";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        if (formKey.currentState!.validate()) {
+                          print(usernameController.text);
+                          print(passwordController.text);
+                        }
+                      },
+                      child: Container(
+                        margin: EdgeInsets.only(top: 24),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 24, vertical: 6),
+                        decoration: BoxDecoration(
+                          color: AppColors.green[600],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Text(
+                          "เข้าสู่ระบบ",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
