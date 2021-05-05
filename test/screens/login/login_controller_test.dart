3c9d7f737f53b615ff6c8503cfb8c06729513b8a
Boonsong Srithong <song@odds.team>
2021-05-05 21:29:13 +0700

    refactor test

--- test/screens/login/login_controller_test.dart
+++ test/screens/login/login_controller_test.dart
@@ -2,27 +2,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:simple_login/screens/login/login_controller.dart';
 
 main() {
-  test("invalid useranme", () {
+  group("validator", () {
     var controller = LoginController();
-    final actual = controller.usernameValidator("");
-    expect(actual, "กรุณาระบุชื่อบัญชี");
-  });
+    test("invalid useranme", () {
+      final actual = controller.usernameValidator("");
+      expect(actual, "กรุณาระบุชื่อบัญชี");
+    });
 
-  test("valid useranme", () {
-    var controller = LoginController();
-    final actual = controller.usernameValidator("test");
-    expect(actual, null);
-  });
+    test("valid useranme", () {
+      final actual = controller.usernameValidator("test");
+      expect(actual, null);
+    });
 
-  test("valid password", () {
-    var controller = LoginController();
-    final actual = controller.usernameValidator("password");
-    expect(actual, null);
-  });
+    test("valid password", () {
+      final actual = controller.passwordValidator("password");
+      expect(actual, null);
+    });
 
-  test("invalid useranme", () {
-    var controller = LoginController();
-    final actual = controller.usernameValidator("");
-    expect(actual, "กรุณาระบุรหัสผ่าน");
+    test("invalid useranme", () {
+      final actual = controller.passwordValidator("");
+      expect(actual, "กรุณาระบุรหัสผ่าน");
+    });
   });
 }
