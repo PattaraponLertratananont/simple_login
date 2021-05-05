e613fc9721cb9260a24be2bb08782bd07fbf8936
MuyoNz <pattaraponler@gmail.com>
2021-05-05 13:55:02 +0700

    const color

---
+++ lib/const/colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // bg - F3F9F1
+  static const Color background = Color(0xFFF3F9F1);
+  static const MaterialColor green = MaterialColor(
+    0xFFE4F0E2,
+    <int, Color>{
+      // green - E4F0E2
+      500: Color(0xFFE4F0E2),
+      // button - 778B73
+      600: Color(0xFF778B73),
+    },
+  );
+}
--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:simple_login/const/colors.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key? key}) : super(key: key);
@@ -11,6 +12,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       body: Container(
         child: Image.asset("assets/cover.png"),
       ),
